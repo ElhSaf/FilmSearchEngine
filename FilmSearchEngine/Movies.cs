df2154ac81ad64a147f3d88905955288f73ddc0d
ElhSaf <elham_abdul@hotmail.com>
2021-12-08 14:59:29 +0100

    search function done

---
+++ FilmSearchEngine/Movies.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmSearchEngine
+{
+    class Movies
+    {
+
+         
+        
+
+            public string homepage { get; set; }
+
+
+            public string original_language { get; set; }
+
+            public string overview { get; set; }
+
+            public string poster_path { get; set; }
+
+            public string release_date { get; set; }
+
+            public int runtime { get; set; }
+
+
+
+            public string title { get; set; }
+
+            public float vote_average { get; set; }
+
+
+            public void DisplayMovie()
+            {
+                Console.WriteLine(" Title: {0}", title);
+                Console.WriteLine(" Runt Time: {0}", runtime);
+                Console.WriteLine(" Release date: {0}", release_date);
+                Console.WriteLine(" Rating: {0}", vote_average);
+                Console.WriteLine(" Overview: {0}", overview);
+            Console.WriteLine($"Poser Link: https://image.tmdb.org/t/p/w500{poster_path}");
+
+            }
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+    }
+}
--- FilmSearchEngine/Program.cs
+++ FilmSearchEngine/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 namespace FilmSearchEngine
 {
     class Program
     {
-        static void Main(string[] args)
+        public static  HttpClient client = new HttpClient();
+        static async Task  Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+            DotNetEnv.Env.TraversePath().Load();
+            string key = Environment.GetEnvironmentVariable("ApiKey");
+            Console.WriteLine("enter Movie Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            string MovieId = $"https://api.themoviedb.org/3/movie/{id}?api_key={key}";
+
+            var respons = await client.GetAsync(MovieId);
+            respons.EnsureSuccessStatusCode();
+
+            string responsContent = await respons.Content.ReadAsStringAsync();
+           
+
+            Movies test = JsonConvert.DeserializeObject<Movies>(responsContent);
+
+            test.DisplayMovie();
+
+            
+
+
         }
     }
 }
